ccbde43d83d4b7e03c37bc0c4ca235e42e17fc5d
Jonathan Riedel <pn@bptv.eu>
2024-05-02 19:23:12 +0200

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,31 @@
+﻿using BepInEx;
+using HarmonyLib;
+using MyBox;
+using System.Collections;
+using UnityEngine;
+
+namespace SaveOnQuit
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded! Applying patch...");
+            Harmony harmony = new Harmony("com.orpticon.SaveOnQuit");
+            harmony.PatchAll();
+        }
+    }
+    public static class SaveOnQuitPatch
+    {
+
+        [HarmonyPatch(typeof(EscapeMenuManager), "Quit")]
+        public static class EscapeMenuManager_Quit_Patch
+        {
+            public static void Prefix()
+            {
+                Singleton<SaveManager>.Instance.Save();
+            }
+        }
+    }
+}
